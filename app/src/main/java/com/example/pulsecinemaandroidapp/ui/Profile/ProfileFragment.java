276f556abc21c0dc9a10f41abd423c0c7b0437d9
Hashan Ruchira <hashanruchira98@gmail.com>
2023-11-03 16:05:34 +0530

    initial commit

--- app/src/main/java/com/example/pulsecinemaandroidapp/ui/Profile/ProfileFragment.java
+++ app/src/main/java/com/example/pulsecinemaandroidapp/ui/Profile/ProfileFragment.java
@@ -1,4 +1,4 @@
-package com.example.pulsecinemaandroidapp.ui.notifications;
+package com.example.pulsecinemaandroidapp.ui.Profile;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,18 +10,19 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.pulsecinemaandroidapp.databinding.FragmentNotificationsBinding;
+import com.example.pulsecinemaandroidapp.databinding.FragmentProfileBinding;
 
-public class NotificationsFragment extends Fragment {
 
-    private FragmentNotificationsBinding binding;
+public class ProfileFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        ProfileViewModel notificationsViewModel =
+                new ViewModelProvider(this).get(ProfileViewModel.class);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textNotifications;
