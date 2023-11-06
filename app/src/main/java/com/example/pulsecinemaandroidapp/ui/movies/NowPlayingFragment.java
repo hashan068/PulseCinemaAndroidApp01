6adafb7cc873536698faccb50775a855642fa664
Hashan Ruchira <hashanruchira98@gmail.com>
2023-11-06 08:39:15 +0530

    Revert "commit"
    This reverts commit 0f58cf28fc5e38992fefb39b7328a275f86c39e7.

--- app/src/main/java/com/example/pulsecinemaandroidapp/ui/movies/NowPlayingFragment.java
+++ app/src/main/java/com/example/pulsecinemaandroidapp/ui/movies/NowPlayingFragment.java
@@ -1,6 +1,5 @@
 package com.example.pulsecinemaandroidapp.ui.movies;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,28 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
-import com.example.pulsecinemaandroidapp.MovieDetailsActivity;
 import com.example.pulsecinemaandroidapp.R;
 
 public class NowPlayingFragment extends Fragment {
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_now_playing, container, false);
-
-        Button buyTicketButton = rootView.findViewById(R.id.buttonBuyTicket01);
-        buyTicketButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Handle button click here
-                // You can navigate to MovieDetailsActivity here
-                startActivity(new Intent(getActivity(), MovieDetailsActivity.class));
-            }
-        });
-
-        return rootView;
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_now_playing, container, false);
     }
 }
