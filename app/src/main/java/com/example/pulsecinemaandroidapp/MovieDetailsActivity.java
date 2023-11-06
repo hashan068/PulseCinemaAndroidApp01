0be5d10c9e0cebb0db19ccbe9b7be3531f83a109
Hashan Ruchira <hashanruchira98@gmail.com>
2023-11-06 10:25:16 +0530

    commit

---
+++ app/src/main/java/com/example/pulsecinemaandroidapp/MovieDetailsActivity.java
@@ -0,0 +1,42 @@
+package com.example.pulsecinemaandroidapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.pulsecinemaandroidapp.R;
+import com.example.pulsecinemaandroidapp.SelectDateActivity;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+
+        // Back Button
+        ImageView backButton = findViewById(R.id.left_icon);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish(); // Close the current activity and go back to the previous one
+            }
+        });
+
+        // Buy Ticket Button
+        Button buyTicketButton = findViewById(R.id.buttonBuyTicket);
+        buyTicketButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle buy ticket button click
+                // For example, you can open a new activity for ticket booking
+                Intent intent = new Intent(MovieDetailsActivity.this, SelectDateActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/pulsecinemaandroidapp/SelectShowTimeActivity.java
@@ -0,0 +1,14 @@
+package com.example.pulsecinemaandroidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectShowTimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_show_time);
+    }
+}
