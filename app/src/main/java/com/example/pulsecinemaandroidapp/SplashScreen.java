38f8552264058ed8373be983e293326b28132717
Sudheera Gamalath <114352434+Sudheera06@users.noreply.github.com>
2023-11-11 16:12:25 +0530

    1

--- app/src/main/java/com/example/pulsecinemaandroidapp/SplashScreen.java
+++ app/src/main/java/com/example/pulsecinemaandroidapp/SplashScreen.java
@@ -1,8 +1,13 @@
 package com.example.pulsecinemaandroidapp;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.pulsecinemaandroidapp.ui.login.LoginActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent( SplashScreen.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
     }
 }
